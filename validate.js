49c84ae4867a10ff4751906eb8f86b46b7d70d93
Tiffany Conroy <tiffany.conroy@gmail.com>
2011-09-28 18:34:03 +0200

    warning handled by invalid eventListener

--- validate.js
+++ validate.js
@@ -2,11 +2,9 @@ var form = document.forms[0];
 
 form.addEventListener('submit', function (event) {
 
-  // Safari gets here, because it fires a submit event at the form when the submit button is clicked
-  // Chrome and Firefox do not fire a submit when the submit button is clicked but the form is not valid
-  // The spec allows both interpretations
-
   event.preventDefault();
+
+  removeHints(form);
 
   if (form.checkValidity()) {
     // Now do Ajax stuff here!
@@ -19,57 +17,47 @@ form.addEventListener('submit', function (event) {
 
 });
 
-var output = document.createElement('div');
-document.body.appendChild(output);
-
 // The confirmation happens on success
 var confirm = function (response) {
+  var output = document.createElement('div');
   output.innerHTML = response;
-};
-
-var warn = function (form) {
-  console.log('test');
-  var inputs = Array.prototype.slice.apply(form.elements);
-  inputs.forEach(function (input) {
-    var message = input.validationMessage;
-    var label, hint;
-    if (!input.validity.valid) {
-      if (!input.validity.customError && input.dataset.customValidationMessage) {
-        message = input.dataset.customValidationMessage;
-      }
-      hint = document.createElement('span');
-      hint.innerText = message;
-      form.insertBefore(hint, input);
-      console.log(message);
-    }
-  });
+  document.body.appendChild(output);
 };
 
 var submitButton = form.querySelector('[type=submit]');
 
 submitButton.addEventListener('click', function (event) {
 
-  // Chrome and Firefox default: checkValidity of form
-  // Safari default: submit form
+  // Safari default here is to fire a submit event at the form.
+  // Safari never performs the checkValidity() unprompted.
 
-  // Approach: cancel the default, explicity fire submit at form
+  // Chrome/Firefox default here is to checkValidity of form.
+  // Chrome/Firefox submit event not fired (cancelled?)
+  // if checkValidity returns false.
+
+  // It is possible (if the form is one field) to fire
+  // a submit event without this click event happening.
+
+  // Approach:
+  // Cancel the default, then explicity fire submit at form.
+  // This centralizes the checkValidity().
+
+  event.preventDefault();
+
+  var e = document.createEvent('HTMLEvents');
+  e.initEvent('submit', true, true);
+  form.dispatchEvent(e);
 
 });
 
 form.addEventListener('invalid', function (event) {
 
-  // In Chrome and Firefox,
-  // these events are fired on submitButtom click
-  // before submit is fired.
-
-  // In Safari, one must manually execute checkValidity()
-  // within the submit event listener.
-
-  event.preventDefault();
+  event.preventDefault(); // silence hints in Chrome and Firefox
 
   var input = event.srcElement;
   var message = input.validationMessage;
   var hint;
+  form.hints = form.hints || [];
 
   if (!input.validity.valid) {
     if (!input.validity.customError && input.dataset.customValidationMessage) {
@@ -78,6 +66,15 @@ form.addEventListener('invalid', function (event) {
     hint = document.createElement('span');
     hint.innerText = message;
     form.insertBefore(hint, input);
+    form.hints.push(hint);
   }
 
 }, true); // true [useCapture] needed to bubble events from inputs
+
+var removeHints = function (form) {
+  if (!form.hints) return;
+
+  while (form.hints.length) {
+    form.removeChild(form.hints.pop());
+  }
+};
